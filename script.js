9ee5fd3b6c9b4833aee38ee53c047feca6bb3201
Pedro Calcado <pecalcado@gmail.com>
2022-08-17 13:12:17 -0300

    versão0

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Dá-lhe")
+// //const numeroCartas = prompt("Quantas cartas você gostaria de jogar?")
+// if (Number(numeroCartas%2 !== 0)){
+//     return(prompt("Quantas cartas você gostaria de jogar?"))
+// }
+// //const listaCartas =[Number(numeroCartas)]
+// numeroCartas < 14 && numeroCartas >4 = true
+// function virarCarta(){
+// 
+//}
