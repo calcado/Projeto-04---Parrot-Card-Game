f9625f73be8cee16dbdc137de34c8fbb90e25a06
Pedro Calcado <pecalcado@gmail.com>
2022-08-22 17:30:03 -0300

    versãoTentei

--- script.js
+++ script.js
@@ -1,125 +1,99 @@
-const listaCartas = [];
-const numeroCartas = prompt("Quantas cartas você gostaria de jogar?");
-const listaImagens=[
-    "./imagens/bobrossparrot.gif",
-    "./imagens/bobrossparrot.gif",
-    "./imagens/explodyparrot.gif",
-    "./imagens/explodyparrot.gif",
-    "./imagens/fiestaparrot.gif",
-    "./imagens/fiestaparrot.gif",
-    "./imagens/metalparrot.gif",
-    "./imagens/metalparrot.gif",
-    "./imagens/revertitparrot.gif",
-    "./imagens/revertitparrot.gif",
-    "./imagens/tripletsparrot.gif",
-    "./imagens/tripletsparrot.gif",
-    "./imagens/unicornparrot.gif",
-    "./imagens/unicornparrot.gif"
+let cartasClicadas= [];
+let jogadas = 0;
 
-]
+botarJogo()
 
-let numberCartas = Number(numeroCartas);
-const ul = document.querySelector(".listaCartas")
-while(numberCartas < 4 || numberCartas >14 ||isNaN(numberCartas) || Number % 2 === 1){
-  let numberCartas = Number(prompt("Quantas cartas você gostaria de jogar?"));
-  
-    
+function botarJogo(){
+    let listaCartas = [];
+    for(let p = 0; p < 7; p++){
+        listaCartas.push(`<li class="carta" onclick ="escolher(this)">
+        <div class="cartaFrente" identifier="cartaFrente">
+            <img src="imagens/front.png">  
+        </div>
+        <div class="cartaVirada" identifier="cartaVirada>
+            <img src="imagens/parrot${p}.gif">
+        </div>
+        </li>`);
+    }
+    let numeroDeCartas = prompt("Escolha entre 4 e 14 e que seja par");
+    const jogo = document.querySelector("ul");
+    jogo.innerHTML = "";
+    jogadas = 0;
+    let arranjo= [];
+    if(numeroDeCartas >= 4 && numeroDeCartas <= 14 && numeroDeCartas%2 == 0){
+        jogo.style.gridTemplateColumns = `repeat(${numeroDeCartas/2}, 1fr)`;
+
+        for(let i = 0; i < numeroDeCartas/2; i++){ 
+          arranjo.push(listaCartas[i]);
+          arranjo.push(listaCartas[i]);
+        }
+        arranjo.sort(shuffle);
+        for(let j = 0; j < numeroDeCartas; j++){
+        jogo.innerHTML += arranjo[j];
+        }
+        
+    }
+    else{
+        botarJogo();
+    }
+}
+
+function shuffle() { 
+	return Math.random() - 0.5; 
+}
+
+function escolher(carta){
+    jogadas++;
+    carta.classList.add("selecionada");
+    carta.classList.add("desabilitada");
+    cartasClicadas.push(carta);
+    if(cartasClicadas.length === 2){
+        desabilitarCartas();
+        if(cartasClicadas[0].innerHTML === cartasClicadas[1].innerHTML){
+            cartasIdenticas();
+        }
+        else{
+            setTimeout(cartasDiferentes, 2000);
+        }
+    }
+    let pontoS = document.querySelectorAll(".ponto");
+    let conjuntoCartas = document.querySelectorAll(".carta");
+    if(pontos.length === conjuntoCartas.lenght){
+        setTimeout(fim, 500);
+    }
+}
+function cartasIdenticas(){
+    liberarCartas();
+    cartasClicadas[0].classList.add("ponto");
+    cartasClicadas[1].classList.add("ponto");
+    cartasClicadas[0].classList.remove("selecionada");
+    cartasClicadas[1].classList.remove("selecionada");
+    cartasClicadas = [];
+
+}
+
+function cartasDiferentes(){
+    liberarCartas();
+    cartasClicadas[0].classList.remove("selecionada");
+    cartasClicadas[1].classList.remove("selecionada");
+    cartasClicadas = [];
+}
+
+function liberarCartas(){
+    let conjuntoCartas = document.querySelectorAll(".carta");
+    for(let i = 0; i < conjuntoCartas.length; i++){
+        conjuntoCartas[i].classList.add("desabilitada");
+    }
+}
+
+function desabilitarCartas(){
+    let conjuntoCartas = document.querySelectorAll(".carta");
+    for(let i = 0; i < conjuntoCartas.length; i++){
+        conjuntoCartas[i].classList.add("desabilitada");
+    }
+
+}
+function fim(){
+    alert(`Você ganhou em ${jogadas} jogadas`);
     
 }
-for (i = 0; i < numberCartas; i++) {
-    ul.innerHTML += `<li class="carta" onclick="viraCarta"(this)><div><img src="./imagens/front.png" class="frenteCarta"></div><div></div></li>`
-}4
-
-// function viraCarta(this){
-//     const carta = document.querySelector('.frenteCarta');
-//      carta.classList.toggle('.virada');
-
-// }
-
-
-// <img src="${listaImagens[i]}"></img>
-
-
-// for (i = 0; i < numberCartas; i++) {
-//     ul.innerHTML += `<li class="carta" onclick="viraCarta"(this)><div><img src="./imagens/front.png" class="frenteCarta"></div><div class="cartaVirada"><img src="${listaImagens[i]}"</div></li>`
-//     function viraCarta(this){
-//         alert('oi')}
-// }  
-
-
-//     const carta = document.querySelector(".frenteCarta");
-//     carta.classList.toggle('cartaVirada');
-// }
-//     if(cartaClicada===cartaClicada2){
-//         function()
-//     }
-// }
-
-// function checarCarta(){
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-    // (`
-    // //         <li class="carta" onclick="viraCarta(this)">
-    // //         <div class="frenteCarta">
-    // //                 <img src="./imagens/front.png class="frenteCarta"></div>
-    // //                 <div class="cartaVirada">
-    // //                 <img src="" >
-    // //             </div>
-    // //         </li> `)
-
-    
-
-// let contador = 0;
-// while(contador<numberCartas){
-//         lista = document.querySelector(".listaCartas")
-//         lista.innerHTML =+ `
-//         <li class="carta" onclick="viraCarta(this)">
-//         <div class="frenteCarta">
-//                 <img src="./imagens/front.png class="frenteCarta"></div>
-//                 <div class="cartaVirada">
-//                 <img src="" >
-//             </div>
-//         </li> `    
-        
-//     }
-    
-
-// function virarCarta(){
-
-
-//}
-//function checaCarta(){
-// if(cartaClicada === )     
-
-//} 
-// function fimDeJogo(){
-
-// }
-
-
-
-
-
-
-
-
-    //     return(prompt("Quantas cartas você gostaria de jogar?"))
-
-// }
-
-// function virarCarta(){
-//     const elemento = querySelector('.cartaFrente');
-//      img.src=""
-// }
