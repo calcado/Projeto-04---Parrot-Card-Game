b7ce59e599da13bbffb99503c169cd3c285dd16a
calcado <pecalcado@gmail.com>
2024-04-02 18:19:05 -0300

    create Deck and Board

---
+++ script.js
@@ -0,0 +1,76 @@
+let cardNumber;
+let text = "Escolha entre 4 e 14 cartas para jogar, sendo um número par";
+const picturesCards = [
+  "parrot0",
+  "parrot1",
+  "parrot2",
+  "parrot3",
+  "parrot4",
+  "parrot5",
+  "parrot6",
+];
+
+const deck = [];
+
+function validation() {
+  if (
+    isNaN(cardNumber) ||
+    cardNumber % 2 !== 0 ||
+    cardNumber < 4 ||
+    cardNumber > 14
+  ) {
+    return true;
+  }
+}
+
+function separative() {
+  return Math.random() - 0.5;
+}
+
+function setBoard() {
+  const board = document.querySelector(".board");
+  
+  for (let i = 0; i < deck.length; i++) {
+    
+    board.innerHTML +=  `
+            <li class="card" onclick='turnCard(this)'>
+                <div class='front-face cardFace'>
+                    <img src='imagens/front.png'>
+                </div>
+                <div class='back-face cardFace'>
+                    <img src='imagens/${deck[i]}.gif'>
+                </div>
+            </li> `;
+  }
+}
+
+function createDeck() {
+  for (let i = 0; i < cardNumber / 2; i++) {
+    let card = picturesCards[i];
+    deck.push(card);
+    deck.push(card);
+  }
+
+  console.log(deck)
+
+  deck.sort(separative);
+
+  setBoard();
+}
+
+
+function numberOfCards() {
+  cardNumber = Number(prompt(text));
+  while (validation()) {
+    alert("Quantidade de cartas inválida");
+    cardNumber = Number(
+      prompt(`Por favor, escolha um número par entre 4 e 14`)
+    );
+  }
+  console.log("arrumando o tabuleiro...");
+  
+  createDeck();
+
+}
+
+numberOfCards();
